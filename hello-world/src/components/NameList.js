bd2d84c0b39a8893e3fdc9621fdb0ec18a7f07f1
Peyton Bechard <pbechard@my.yorku.ca>
2022-07-19 20:59:14 +0800

    Changes NameList component to learn about using index as a key for list rendering in hello-world app

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -2,30 +2,33 @@ import React from 'react';
 import Person from './Person';
 
 function NameList() {
-  const persons = [
-    {
-      id: 1,
-      name: 'Jack',
-      age: 30,
-      skill: 'React',
-    },
-    {
-      id: 2,
-      name: 'Jill',
-      age: 35,
-      skill: 'Vue',
-    },
-    {
-      id: 3,
-      name: 'Joe',
-      age: 25,
-      skill: 'Angular',
-    },
-  ];
-  const personsList = persons.map((person) => (
-    <Person key={person.id} person={person} />
-  ));
-  return <div>{personsList}</div>;
+  //   const persons = [
+  //     {
+  //       id: 1,
+  //       name: 'Jack',
+  //       age: 30,
+  //       skill: 'React',
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'Jill',
+  //       age: 35,
+  //       skill: 'Vue',
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'Joe',
+  //       age: 25,
+  //       skill: 'Angular',
+  //     },
+  //   ];
+  //   const personsList = persons.map((person) => (
+  //     <Person key={person.id} person={person} />
+  //   ));
+  //   return <div>{personsList}</div>;
+  const names = ['Rick', 'Ron', 'Rory', 'Rick'];
+  const nameList = names.map((name, index) => <h2 key={index}>{name}</h2>);
+  return <div>{nameList}</div>;
 }
 
 export default NameList;
