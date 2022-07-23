eac3ecbaad3120d0d5356e9e66a51844c9b27b5c
Peyton Bechard <pbechard@my.yorku.ca>
2022-07-23 16:03:19 +0800

    Adds new modules to hello-world app to illustrate different methods of applying css stylesheets to react components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
-import NameList from './components/NameList';
+import Inline from './components/Inline';
+import Stylesheet from './components/Stylesheet';
+import './appStyles.css';
+import styles from './appStyles.module.css';
 
 function App() {
   return (
@@ -27,7 +30,12 @@ function App() {
       {/* <EventBind /> */}
       {/* <ParentComponent /> */}
       {/* <UserGreeting /> */}
-      <NameList />
+      {/* <NameList /> */}
+      <Stylesheet primary={true} />
+      <Stylesheet primary={false} />
+      <Inline />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
     </div>
   );
 }
